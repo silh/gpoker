29b951f0c3ed99cfa1045d20b05a7975b174a1bc
Ilia Beliaev <i.beliaev@activevideo.com>
2022-05-13 22:59:52 +0200

    Added some locking.

--- main.go
+++ main.go
@@ -4,14 +4,12 @@ import (
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
+	"strconv"
+	"sync"
 )
 
-var counter int64 // FIXME of course this is not thread safe
-
-var games map[int64]Poker // FIXME of course this is not thread safe
-
 type Poker struct {
-	ID      int64    `json:"id"`
+	ID      uint64   `json:"id"`
 	Players []Player `json:"players"`
 }
 
@@ -25,35 +23,54 @@ type CreatePokerRequest struct {
 }
 
 func main() {
-	games = make(map[int64]Poker)
-
 	app := gin.Default()
+	dealer := Dealer{
+		counter: 0,
+		games:   make(map[uint64]Poker),
+	}
 
-	app.POST("/game", createGame)
-	app.GET("/game/:id", getGame)
+	app.POST("/game", dealer.createGame)
+	app.GET("/game/:id", dealer.getGame)
 	if err := app.Run(); err != nil {
 		log.Fatalf("Error = %s", err)
 	}
 }
 
-func createGame(c *gin.Context) {
+// Dealer controls all games.
+type Dealer struct {
+	counter uint64
+	games   map[uint64]Poker
+	lock    sync.RWMutex // protects counter and games
+
+}
+
+func (d *Dealer) createGame(c *gin.Context) {
+	d.lock.Lock()
+	defer d.lock.Unlock()
 	var gameReq CreatePokerRequest
 	if err := c.Bind(&gameReq); err != nil {
 		_ = c.AbortWithError(http.StatusBadRequest, err)
 		return
 	}
 	poker := Poker{
-		ID:      counter,
+		ID:      d.counter,
 		Players: []Player{gameReq.Player},
 	}
-	counter++
-	games[poker.ID] = poker
+	d.counter++
+	d.games[poker.ID] = poker
 	c.JSON(http.StatusCreated, &poker)
 }
 
-func getGame(c *gin.Context) {
-	id := c.GetInt64("id")
-	poker, ok := games[id]
+func (d *Dealer) getGame(c *gin.Context) {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+	idStr := c.Param("id")
+	id, err := strconv.ParseUint(idStr, 10, 0)
+	if err != nil {
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+	poker, ok := d.games[id]
 	if !ok {
 		c.AbortWithStatus(http.StatusNotFound)
 		return
