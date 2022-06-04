42d033646a1337f8d07c5a265a072c3891ab7735
Ilia Beliaev <i.beliaev@activevideo.com>
2022-06-04 22:50:05 +0200

    Added vote.

--- go.mod
+++ go.mod
@@ -3,9 +3,13 @@ module gpoker
 go 1.18
 
 require (
+	github.com/gin-gonic/gin v1.7.7
+	github.com/stretchr/testify v1.7.1
+)
+
+require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.7.7 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
@@ -16,12 +20,11 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.1.0 // indirect
-	github.com/stretchr/testify v1.7.1 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.0.0-20220427172511-eb4f295cb31f // indirect
 	golang.org/x/sys v0.0.0-20220502124256-b6088ccd6cba // indirect
 	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
--- main.go
+++ main.go
@@ -9,17 +9,29 @@ import (
 )
 
 type Poker struct {
-	ID      uint64   `json:"id"`
-	Players []Player `json:"players"`
+	ID      uint64            `json:"id"`
+	Players map[uint64]Player `json:"players"`
 }
 
 type Player struct {
-	ID   int64  `json:"id"`
+	ID   uint64 `json:"id"`
 	Name string `json:"name"`
+	Vote uint64 `json:"vote"`
 }
 
+// CreatePokerRequest to start a game.
 type CreatePokerRequest struct {
 	Player Player `json:"player"`
+}
+
+// JoinPokerRequest to join a game.
+type JoinPokerRequest struct {
+	Player Player `json:"player"`
+}
+
+// VoteRequest for player's vote.
+type VoteRequest struct {
+	Vote uint64 `json:"vote"`
 }
 
 func main() {
@@ -30,7 +42,9 @@ func main() {
 	}
 
 	app.POST("/game", dealer.createGame)
-	app.GET("/game/:id", dealer.getGame)
+	app.GET("/game/:gameId", dealer.getGame)
+	app.PUT("/game/:gameId/join", dealer.joinGame)
+	app.POST("/game/:gameId/player/:playerId/vote", dealer.vote)
 	if err := app.Run(); err != nil {
 		log.Fatalf("Error = %s", err)
 	}
@@ -54,7 +68,7 @@ func (d *Dealer) createGame(c *gin.Context) {
 	}
 	poker := Poker{
 		ID:      d.counter,
-		Players: []Player{gameReq.Player},
+		Players: map[uint64]Player{gameReq.Player.ID: gameReq.Player},
 	}
 	d.counter++
 	d.games[poker.ID] = poker
@@ -64,9 +78,8 @@ func (d *Dealer) createGame(c *gin.Context) {
 func (d *Dealer) getGame(c *gin.Context) {
 	d.lock.Lock()
 	defer d.lock.Unlock()
-	idStr := c.Param("id")
-	id, err := strconv.ParseUint(idStr, 10, 0)
-	if err != nil {
+	id, ok := extractUint64(c, "gameId")
+	if !ok {
 		c.AbortWithStatus(http.StatusBadRequest)
 		return
 	}
@@ -76,4 +89,66 @@ func (d *Dealer) getGame(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, &poker)
+}
+
+func (d *Dealer) joinGame(c *gin.Context) {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+	var joinReq JoinPokerRequest
+	if err := c.BindJSON(&joinReq); err != nil {
+		_ = c.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+	id, ok := extractUint64(c, "gameId")
+	if !ok {
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+	game, ok := d.games[id]
+	if !ok {
+		c.AbortWithStatus(http.StatusNotFound)
+		return
+	}
+	game.Players[joinReq.Player.ID] = joinReq.Player
+	c.JSON(http.StatusOK, &game)
+}
+
+func (d *Dealer) vote(c *gin.Context) {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+	gameId, ok := extractUint64(c, "gameId")
+	if !ok {
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+	playerId, ok := extractUint64(c, "playerId")
+	if !ok {
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+	game, ok := d.games[gameId]
+	if !ok {
+		c.AbortWithStatus(http.StatusNotFound)
+		return
+	}
+	player, ok := game.Players[playerId]
+	if !ok {
+		c.AbortWithStatus(http.StatusBadRequest) // player not part of the game
+		return
+	}
+	var voteReq VoteRequest
+	if err := c.BindJSON(&voteReq); err != nil {
+		log.Printf("Failed to parse vote request = %s", err)
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+	player.Vote = voteReq.Vote
+	game.Players[playerId] = player
+	c.JSON(http.StatusOK, game)
+}
+
+func extractUint64(c *gin.Context, name string) (uint64, bool) {
+	idStr := c.Param(name)
+	id, err := strconv.ParseUint(idStr, 10, 0)
+	return id, err == nil
 }
